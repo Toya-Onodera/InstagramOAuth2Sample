9760823cbc466739ea3c00382166c0b814efef79
Toya-Onodera <gtrzexel@gmail.com>
2021-12-18 23:14:14 +0900

    Add: ルーティングの設定

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { IndexPageComponent } from "./components/pages/IndexPageComponent";
 
 export const App: React.VFC = () => {
-  return <IndexPageComponent />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          <IndexPageComponent />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
